f8d02df6b2de620a731b3b7bba709ac61b51dc2e
david.dimon <david_dimon@hotmail.com>
2018-06-25 21:55:33 -0300

    :hammer: removed method readUrl(useless)

--- server/src/main/java/com/involves/selecao/service/ProcessadorAlerta.java
+++ server/src/main/java/com/involves/selecao/service/ProcessadorAlerta.java
@@ -44,26 +44,6 @@ public class ProcessadorAlerta {
         }
     }
 
-    private String readUrl(String urlString) throws Exception {
-        BufferedReader reader = null;
-        try {
-            URL url = new URL(urlString);
-            reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            StringBuffer buffer = new StringBuffer();
-            int read;
-            char[] chars = new char[1024];
-            while ((read = reader.read(chars)) != -1)
-                buffer.append(chars, 0, read);
-
-            return buffer.toString();
-        } finally {
-            if (reader != null)
-                reader.close();
-        }
-
-    }
-
-
     public Alerta getAlertaSituacao(Pesquisa pesquisa, Resposta resposta) {
         if (resposta == null || resposta.getResposta() == null
                 && pesquisa == null) {
